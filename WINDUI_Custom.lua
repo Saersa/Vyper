d5c0546e11cb1ac8d5836990cef6e0ac85136992
Saersa <142904335+Saersa@users.noreply.github.com>
2025-10-17 01:35:49 +0200

    Update WINDUI_Custom.lua

--- WINDUI_Custom.lua
+++ WINDUI_Custom.lua
@@ -614,7 +614,7 @@ Colors={
 }.IconFrame
 J.Parent=H
 elseif string.find(u,"http")then
-local J="WindUI/"..B.."/assets/."..C.."-"..v..".png"
+local J="Vyper/"..B.."/assets/."..C.."-"..v..".png"
 local L,M=pcall(function()
 task.spawn(function()
 if not isfile(J)then
