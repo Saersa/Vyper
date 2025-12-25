972fe5c154109628be00b707ebe0b115a4271f01
Saersa <142904335+Saersa@users.noreply.github.com>
2025-12-25 06:49:08 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local url = "https://raw.githubusercontent.com/Saersa/EGH_2.0/refs/heads/main/Ga
 
 local games = {
 	  [13076380114] = "HBG"; -- GameID, script name
-    
+      [3652625463] = "Lifting Simulator";
 	
 	
 }
